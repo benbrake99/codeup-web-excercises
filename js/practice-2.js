eb6779cac280f5b00ad903cdb9b69f2b8f93b879
Ben Brake <ben.brake99@gmail.com>
2022-03-16 22:55:49 -0500

    practice

--- js/practice-2.js
+++ js/practice-2.js
@@ -7,20 +7,16 @@ var result = multiplyNumbers(5, 10);
 console.log(result);*/
 
 
-function howIsTheTemp (temp){
+function testNum (a){
     var result;
-    if(temp > 80){
-        result = "whew, it is really hot today";
-    } else if (temp > 60 && temp < 79){
-        result = "it is a very nice day today!"
-        } else {
-            result = "Stay Home! it's cold outside!"
-        }{
-        return result;
+    if (a > 0){
+        result = 'positive'
+    } else {
+        result = 'negative'
     }
-        howIsTheTemp(95)
-        console.log(temp);
-
+    return result;
 
 }
+    console.log(testNum(-2));
+
 
