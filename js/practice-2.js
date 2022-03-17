7c6550b733e0dd1f8213614cc88d3f3169da74f6
Ben Brake <ben.brake99@gmail.com>
2022-03-16 22:40:11 -0500

    practice

--- js/practice-2.js
+++ js/practice-2.js
@@ -1,6 +1,26 @@
 
+/*
 function multiplyNumbers(x,y){
     return x * y;
 }
 var result = multiplyNumbers(5, 10);
-console.log(result);
+console.log(result);*/
+
+
+function howIsTheTemp (temp){
+    var result;
+    if(temp > 80){
+        result = "whew, it is really hot today";
+    } else if (temp > 60 && temp < 79){
+        result = "it is a very nice day today!"
+        } else {
+            result = "Stay Home! it's cold outside!"
+        }{
+        return result;
+    }
+        howIsTheTemp(95)
+        console.log(temp);
+
+
+}
+
