f6dc22f6883d30406aaeb98399ecc00234de8513
Ben Brake <ben.brake99@gmail.com>
2022-03-17 22:37:39 -0500

    practice for test

--- js/practice-2.js
+++ js/practice-2.js
@@ -1,35 +1,46 @@
+//
+// /*
+// function multiplyNumbers(x,y){
+//     return x * y;
+// }
+// var result = multiplyNumbers(5, 10);
+// console.log(result);*/
+//
+// /*let a = prompt("put in a positive or negative number.")
+// function testNum (a){
+//     var result;
+//     if (a > 0){
+//         result = 'positive'
+//     } else {
+//         result = 'negative'
+//     }
+//     return result;
+//
+// }
+//     console.log(testNum(a));*/
+//
+// var a = prompt("enter number between 1-100 for 'A'.");
+// var b = prompt("enter another number between 1-100 for 'B'.");
+// function multiply(a,b){
+//     var result;
+//     if(a > b){
+//         result = "a is greater than b"
+//     } else {
+//         result = "b is greater than a"
+//     }
+//     return result;
+// }
+// console.log(multiply(a,b));
 
-/*
-function multiplyNumbers(x,y){
-    return x * y;
-}
-var result = multiplyNumbers(5, 10);
-console.log(result);*/
-
-/*let a = prompt("put in a positive or negative number.")
-function testNum (a){
-    var result;
-    if (a > 0){
-        result = 'positive'
-    } else {
-        result = 'negative'
-    }
-    return result;
-
-}
-    console.log(testNum(a));*/
-
-var a = prompt("enter number between 1-100 for 'A'.");
-var b = prompt("enter another number between 1-100 for 'B'.");
-function multiply(a,b){
-    var result;
-    if(a > b){
-        result = "a is greater than b"
-    } else {
-        result = "b is greater than a"
-    }
-    return result;
-}
-console.log(multiply(a,b));
-
-
+// var pizzaPref = prompt("what kind of pizza would you like? 1. pineapple and hot sauce? 2. cheese 3. other");
+// switch (pizzaPref){
+//     case "pineapple and hot sauce":
+//         console.log("That's a great choice!");
+//         break;
+//     case "cheese":
+//         console.log("good choice, but very vanilla");
+//         break;
+//     default:
+//         console.log(pizzaPref + "looks like you want to pick your own!");
+//         break;
+// }
