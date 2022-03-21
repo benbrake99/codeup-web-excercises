176fedde266595feb8c6e6ff1bcae03080345187
Ben Brake <ben.brake99@gmail.com>
2022-03-21 12:16:01 -0500

    loop exercise

---
+++ js/for_loops.js
@@ -0,0 +1,45 @@
+
+//
+// const number = parseInt(prompt('Enter a number'));
+//
+// for(var i = 1; i <= 10; i++){
+//     const result = i * number;
+//     console.log('${number} * ${i} = ${result}');
+// }
+
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+function showMultiplicationTable(input){
+
+    console.log(input + " x 1 = " + input * 1);
+    console.log(input + " x 2 = " + input * 2);
+    console.log(input + " x 3 = " + input * 3);
+    console.log(input + " x 4 = " + input * 4);
+    console.log(input + " x 5 = " + input * 5);
+    console.log(input + " x 6 = " + input * 6);
+    console.log(input + " x 7 = " + input * 7);
+    console.log(input + " x 8 = " + input * 8);
+    console.log(input + " x 9 = " + input * 9);
+    console.log(input + " x 10 = " + input * 10);
+
+}
+showMultiplicationTable(7);
+//
+// For example, showMultiplicationTable(7) should output
+//
+//
+// 7 x 1 = 7
+// 7 x 2 = 14
+// 7 x 3 = 21
+// 7 x 4 = 28
+// 7 x 5 = 35
+// 7 x 6 = 42
+// 7 x 7 = 49
+// 7 x 8 = 56
+// 7 x 9 = 63
+// 7 x 10 = 70
+
+
+
+
+
+
