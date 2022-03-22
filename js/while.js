6f5e16ebaab6916dea7f0453bfe64a452173bbd9
Ben Brake <ben.brake99@gmail.com>
2022-03-22 10:31:48 -0500

    practice exercise

--- js/while.js
+++ js/while.js
@@ -1,4 +1,28 @@
-var i = 2;
-var sumOfI =
 
-while(i * 2; i <= 65536;
+
+var i = 1;
+while (i <= 16) {
+    console.log(Math.pow(2,i)); i++
+}
+
+
+
+
+
+//come back and finish this practice exercise...fun
+//
+// var base = prompt("Enter a number for the base of the triangle.");
+// console.log(base + "the base is ")
+// var high = prompt("Enter a number for the heighth of the triangle");
+// console.log(high + " The heighth is ")
+// var len = prompt("Enter a number for the length of the triangle");
+// console.log(len + " The length is ")
+// parseInt(base);
+// parseInt(high);
+// parseInt(len);
+// function triArea(base,heighth,len){
+//    var area = base * heighth * len;
+//     console.log(area + "the area of the triangle is ")
+//
+// }
+// triArea()
