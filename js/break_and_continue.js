2d4f2e5d43dc46654109be5579af3378921a0d48
Ben Brake <ben.brake99@gmail.com>
2022-03-21 23:54:24 -0500

    loop exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,7 @@
 
 var pickNum = prompt("Pick any odd number between 1-50");
 var selected = parseInt(pickNum);
+console.log("The number to skip is " + selected);
 console.log(' odd number are ');
 for(var i = 1; i < 50; i += 2) {
     if(i === selected){
---
+++ js/while.js
@@ -0,0 +1,4 @@
+var i = 2;
+var sumOfI =
+
+while(i * 2; i <= 65536;
