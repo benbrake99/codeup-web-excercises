05972261f6fd679d6dba6b098d3a54d74f7289cc
Ben Brake <ben.brake99@gmail.com>
2022-03-23 15:44:00 -0500

    arrays exercise

--- js/while.js
+++ js/while.js
@@ -1,47 +1,47 @@
-
-
-// var i = 1;
-// while (i <= 16) {
-//     console.log(Math.pow(2,i)); i++
+//
+//
+// // var i = 1;
+// // while (i <= 16) {
+// //     console.log(Math.pow(2,i)); i++
+// // }
+// //
+//
+//
+//
+//
+// //come back and finish this practice exercise...fun
+// //
+// // var base = prompt("Enter a number for the base of the triangle.");
+// // console.log(base + "the base is ")
+// // var high = prompt("Enter a number for the heighth of the triangle");
+// // console.log(high + " The heighth is ")
+// // var len = prompt("Enter a number for the length of the triangle");
+// // console.log(len + " The length is ")
+// // parseInt(base);
+// // parseInt(high);
+// // parseInt(len);
+// // function triArea(base,heighth,len){
+// //    var area = base * heighth * len;
+// //     console.log(area + "the area of the triangle is ")
+// //
+// // }
+// // triArea()
+//
+// var count = console.log(Math.floor(Math.random() * (100 - 50 + 1)) + min);
+//
+// do{
+//
+//
 // }
 //
-
-
-
-
-//come back and finish this practice exercise...fun
+// do{ var clientCones = console.log(Math.floor(Math.random() * (5 - 1 + 1)) + min;) > 1}
 //
-// var base = prompt("Enter a number for the base of the triangle.");
-// console.log(base + "the base is ")
-// var high = prompt("Enter a number for the heighth of the triangle");
-// console.log(high + " The heighth is ")
-// var len = prompt("Enter a number for the length of the triangle");
-// console.log(len + " The length is ")
-// parseInt(base);
-// parseInt(high);
-// parseInt(len);
-// function triArea(base,heighth,len){
-//    var area = base * heighth * len;
-//     console.log(area + "the area of the triangle is ")
+// var result = conesToSell / clientCones;
+// console.log(result)
+// {while (conesToSell; clientCones / conesToSell) clientCones++}
 //
-// }
-// triArea()
-
-var count = console.log(Math.floor(Math.random() * (100 - 50 + 1)) + min);
-
-do{
-
-
-}
-
-do{ var clientCones = console.log(Math.floor(Math.random() * (5 - 1 + 1)) + min;) > 1}
-
-var result = conesToSell / clientCones;
-console.log(result)
-{while (conesToSell; clientCones / conesToSell) clientCones++}
-
-
-
+//
+//
 
 
 
@@ -59,5 +59,9 @@ console.log(result)
 //     }
 // }
 
+var schoolGames =( ["dodgeball", "kickball", "hopscotch", "tag", "hide" +
+" and seek"]
 
-
+var schoolGames.forEach(function(games);
+{
+console.log(games)}
