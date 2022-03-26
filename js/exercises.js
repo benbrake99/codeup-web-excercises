c10c0bc981655de040fb05113abb3651a035e12d
Ben Brake <ben.brake99@gmail.com>
2022-03-25 20:35:31 -0500

    exercises

--- js/exercises.js
+++ js/exercises.js
@@ -17,38 +17,36 @@
 //     - The price of a product? = number
 
 
-        Operators Exercise
+//         Operators Exercise
+//
+// ## Exercises
+//
+// For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in the Chrome console.
+//
+//     `'1' + 2` = '12'
+//     `typeof '1' + 2` = "string2"
+//     `6 % 4` = 2
+//     `'3 + 4 is ' + 3 + 4  = '3 + 4 is 34' = string
+// // how could you make this produce the correct output?`
 
-## Exercises
-
-For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in the Chrome console.
-
-    `'1' + 2` = '12'
-    `typeof '1' + 2` = "string2"
-    `6 % 4` = 2
-    `'3 + 4 is ' + 3 + 4  = '3 + 4 is 34' = string
-// how could you make this produce the correct output?`
-    `0 < 0`
-    'false' == false` = 'false' === false
- `// how could you make this produce the correct output?`
-        `0 < 0`
-        `'false' == false`
-        `true == 'true'`
-        `5 >= -5`
-        `!false || false`
-        `true || "42"`
-        `!true && !false`
-        `6 % 5`
-        `5 < 4 && 1 === 1`
-        `typeof 'codeup' === 'string'`
-        `'codeup' === 'codeup' && 'codeup' === 'Codeup'`
-        `4 >= 0 && 1 !== '1'`
-        `6 % 3 === 0`
-        `5 % 2 !== 0`
-
+ // `// how could you make this produce the correct output?`
+ //        `0 <= 0`    = true 'adding ='
+ //        `'false' === false` = true 'adding ='
+ //        `true !== 'true'` = true 'adding !'
+ //        `5 >= -5` = true 'if remove the equal sign'
+ //        `!false || false` = true 'cange to true || !false'
+ //        `'true' || "42"` = true 'adding '' if isString'
+ //        `!true && !false`= true 'if remove ! from either side'
+ //        `6 % 5`   true 'if you add = 1'
+ //        `5 < 4 && 1 === 1` = true 'if you change to > to front portion'
+ //        `typeof 'codeup' === 'string' both are strings
+ //        `'codeup' === 'codeup' && 'codeup' === 'Codeup' '= both = strings''
+ //        `4 >= 0 && 1 !== '1'` = "both are true"
+ //        `6 % 3 === 0` = "true"
+ //        `5 % 2 !== 0` = "true"
 
 
-
+           // Working With Data Types, Operators, and Variables Exercise
 
 
 
