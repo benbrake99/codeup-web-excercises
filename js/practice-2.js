c4af39b426705c1b5cd87d5d85d3cee6495540c7
Ben Brake <ben.brake99@gmail.com>
2022-04-06 11:21:10 -0500

    css exercises

--- js/practice-2.js
+++ js/practice-2.js
@@ -331,24 +331,33 @@
 //     showBookInfo(books[i]);
 // }
 
-
-
-example input: [1,2,3,4,5];
-expected output: 15
-some possible test cases to think about:
-    function should be called ‘sumArr’
-function should return a number
-function should accept an array
-function should return “sum not available” if array is empty
-function should handle non-numbers
-
-var arr = [1, 2, 3, 4, 5];
-var sum = arr.reduce(function (a, b) {
-    return a + b;
-}, 0);
-console.log(sum);
-
-
-
+//
+//
+// example input: [1,2,3,4,5];
+// expected output: 15
+// some possible test cases to think about:
+//     function should be called ‘sumArr’
+// function should return a number
+// function should accept an array
+// function should return “sum not available” if array is empty
+// function should handle non-numbers
+//
+// var arr = [1, 2, 3, 4, 5];
+// var sum = arr.reduce(function (a, b) {
+//     return a + b;
+// }, 0);
+// console.log(sum);
+//
+// example input: [5,8,2,6,9,3,2];
+// expected output: [2,2,3,5,6,8,9] // length: 7
+// some possible test cases to think about:
+//     should be called ‘sortArr’
+// function should return an array
+// function should accept an array
+// BONUS thoughts
+// what would happen if you had negative numbers in your list
+// can you sort and then remove any duplicates (would it be more/less efficient to remove duplicates and then sort?)
+// if your array has non-numbers can you still sort the numbers? ex: input: [3,“b”,8,5,9,true”, 4,“xyz”] expected output: [3,4,5,8,9];
+//
 
 
